da288417e33b8afd77d5a6a4fd62c7a010167b3a
alexis <alexis.auriac@epitech.eu>
2019-05-15 11:26:02 +0200

    The Chi-squared is now displayed

--- app/Main.hs
+++ app/Main.hs
@@ -15,14 +15,32 @@ import DisplayClasses
 displayDistribution :: Float -> IO ()
 displayDistribution p = printf "Distribution:\t\tB(%d, %.4f)\n" (100 :: Int) p
 
+displayChiSquared :: Float -> IO ()
+displayChiSquared x2 = printf "Chi-squared:\t\t%.3f\n" x2
+
+chiSquared :: [Int] -> [Float] -> Float
+chiSquared values thSizes = chiSquared' fValues thSizes 0.0
+    where fValues = map (\x -> fromIntegral x :: Float) values
+
+chiSquared' :: [Float] -> [Float] -> Float -> Float
+chiSquared' [] [] res = res
+chiSquared' (ox:values) (tx:thSizes) res = chiSquared' values thSizes (res+y)
+    where
+        numerator = (ox - tx) ** 2
+        denominator = tx
+        y = numerator / denominator
+
 dowels :: IO ()
 dowels = do
     observedClasses <- processArgv
     let classPacks = packClasses observedClasses
+    let classValues = map sum classPacks
     let p = calcP observedClasses (nbSamples * nbPiecesPerSample)
     let thSizes = packsTheoricalSizes p classPacks
+    let x2 = chiSquared classValues thSizes
     displayClasses classPacks thSizes
     displayDistribution p
+    displayChiSquared x2
 
 main :: IO ()
 main = do
